382ac59cef9b8e96a88c3f46d31b02aeae025dca
github username <github email address>
2020-04-04 13:08:04 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,15 +1,19 @@
 class Shoe
+
     attr_reader :brand
     attr_accessor :color, :size, :material, :condition
 
-
     def initialize (brand)
         @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
     end
 
     def cobble
-        puts "Your shoe is as good as new!"
-        @condition = "new"
+        puts "Your shoe is as good as new!"  
+        self.condition = "new"
     end
 
 end
